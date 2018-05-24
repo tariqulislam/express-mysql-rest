28658478d3d7c718c6d934e4075bdb093299cceb
tariqul islam rony <tariqul.islam.rony@gmail.com>
2018-05-24 23:48:51 +0600

    add the another controller to site

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,7 @@
 const users = require('./users');
+const comments = require('./comments');
 
 module.exports = {
     users,
+    comments
 }
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 
 
 const userController = require('../controllers').users
+const commentController = require('../controllers').comments
 
 module.exports = (app) => {
   app.post('/api/users', userController.create);
   app.get('/api/users', userController.list);
+
+  app.post('/api/comments/:userId/comment', commentController.create )
 }
 /* GET home page. */
